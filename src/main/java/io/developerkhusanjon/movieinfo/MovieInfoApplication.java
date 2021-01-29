6472ca4e500df0689195ab79edbf26bc9de0102d
Khusanjon Tuychiboyev <developerkhusanjon@gmail.com>
2021-01-29 23:06:51 +0500

    init state

---
+++ src/main/java/io/developerkhusanjon/movieinfo/MovieInfoApplication.java
@@ -0,0 +1,13 @@
+package io.developerkhusanjon.movieinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieInfoApplication.class, args);
+    }
+
+}
